6da96539253e9a705480623f8dc30fe0945932eb
Mattia Avanzi <mattiaavanzi@Mattias-Air.home>
2018-12-14 12:11:21 +0100

    create the file with the object that associate roman symbols with their value

---
+++ src/utils/roman-map.js
@@ -0,0 +1,23 @@
+// define the map for binding the roman symbols with 
+// their numeric values.
+// Adding also composed symbols to avoid the 
+// repetition of three equals symbols and in descending order
+// to optimise the conversion algorthm
+
+const RomanMap = {
+    1000 : "M",
+    900 : "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I"
+}
+
+export default RomanMap;
