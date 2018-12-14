2f474a65d9c47bceb434ba7e42da16616ff3cc28
Mattia Avanzi <mattiaavanzi@Mattias-MacBook-Air.local>
2018-12-14 15:31:31 +0100

    create Converter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import romanConverter from './utils/roman-converter';
+import romanMap from './utils/roman-map';
+import Converter from './Converter';
 
 class App extends Component {
+
   render() {
+    const romanConv = romanConverter(romanMap);
     return (
-      <div className="App">       
+      <div className="App"> 
+        <Converter 
+            converter={romanConv}
+          />
       </div>
     );
   }
