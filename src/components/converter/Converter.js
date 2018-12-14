fb48e622125349d9f91b9d0ec9900d1b2acbb927
Mattia Avanzi <mattiaavanzi@Mattias-Air.home>
2018-12-14 22:26:41 +0100

    add style and change title

--- src/components/converter/Converter.js
+++ src/components/converter/Converter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import romanMap from '../../utils/roman-map';
+import './Converter.css';
 
 
 export default class Converter extends React.Component  {
@@ -32,13 +33,18 @@ export default class Converter extends React.Component  {
 
     render() {
        return  (
-        <div>
-            <input type="number" 
+        <div className="converter-container">
+            <h1> Roman numeral converter</h1>
+            <h2>Only numbers from 1 to 3999 are allowed</h2>
+            <input className="converter-input" type="number" 
                 value={this.state.inputValue}
                 onChange={this.setNumber.bind(this)}
-            >            
-            </input>
-            {this.state.finalResult}
+            />
+            <div>
+                <h1>
+                    {this.state.finalResult}
+                </h1>
+            </div>
         </div>
       );  
     }  
